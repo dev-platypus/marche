f3bd415980692a5c9d1a4d0c8486aec398f5a595
Georg Brandl <georg@python.org>
2016-04-09 08:20:55 +0200

    Brush up the democlient to be a usable command line interface.
    Change-Id: Ida90f7c9464dcaecc73e2da7a5713b2362e3c75f
    Reviewed-on: https://review.gerrithub.io/272107
    Reviewed-by: Georg Brandl <georg@python.org>
    Tested-by: Georg Brandl <georg@python.org>

--- tools/democlient.py
+++ tools/democlient.py
@@ -20,31 +20,115 @@
 #
 # Module authors:
 #   Andreas Schulz <aschulz@dev-platypus.org>
+#   Georg Brandl <g.brandl@fz-juelich.de>
 #
 # *****************************************************************************
 
+import cmd
 import sys
-import time
+import ctypes
+import logging
 from os import path
-from pprint import pprint
 
 sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), '..')))
 
 from marche.client import Client
-
-
-def printEvent(event):
-    pprint(vars(event))
-
-c = Client(str(sys.argv[1]) if len(sys.argv) > 1 else 'server.as-schulz.de')
-connectedEvent = c.getServerInfo()
-
-printEvent(connectedEvent)
-
-c.setEventHandler(printEvent)
+from marche.protocol import ConnectedEvent, ServiceListEvent, StatusEvent, \
+    ErrorEvent, RequestServiceListCommand, StartCommand, StopCommand, \
+    RestartCommand
+from marche.jobs import STATE_STR, DEAD, RUNNING, WARNING, STARTING, \
+    STOPPING, INITIALIZING, NOT_RUNNING, NOT_AVAILABLE
+from marche.colors import colorize
 
 try:
-    while True:
-        time.sleep(1)
+    librl = ctypes.cdll[ctypes.util.find_library('readline')]
+except Exception:
+    librl = None
+
+
+class Console(cmd.Cmd):
+    prompt = '\x1b[01mmarche->\x1b[00m '
+    coloring = {
+        DEAD: 'darkred',
+        NOT_RUNNING: 'darkgray',
+        RUNNING: 'darkgreen',
+        WARNING: 'brown',
+        STARTING: 'darkblue',
+        STOPPING: 'darkblue',
+        INITIALIZING: 'darkblue',
+        NOT_AVAILABLE: 'darkgray',
+    }
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        host = str(sys.argv[1]) if len(sys.argv) > 1 else '127.0.0.1'
+        self.client = Client(host, 12132, self.printEvent, logging)
+        connectedEvent = self.client.getServerInfo()
+        self.printEvent(connectedEvent)
+        self.client.send(RequestServiceListCommand())
+
+    def _svcname(self, svc, inst):
+        return '%s.%s' % (svc, inst) if inst else svc
+
+    def _svcinst(self, name):
+        return name.split('.', 1) if '.' in name else (name, '')
+
+    def _fmt_state(self, state):
+        return colorize(self.coloring[state], STATE_STR[state])
+
+    def printEvent(self, event):
+        if isinstance(event, ConnectedEvent):
+            print('\rConnected: daemon version %s, protocol version %s' %
+                  (event.daemon_version, event.proto_version))
+        elif isinstance(event, ErrorEvent):
+            print('\r--> Error: %s' % event.desc)
+        elif isinstance(event, ServiceListEvent):
+            print('\rList of services:')
+            print('%-20s Current state' % 'Service')
+            print('-' * 35)
+            for svc, instances in event.services.items():
+                for inst, info in instances.items():
+                    print('%-20s %s' % (self._svcname(svc, inst),
+                                        self._fmt_state(info['state'])))
+            print('-' * 35)
+        elif isinstance(event, StatusEvent):
+            print('\r--> %s is now %s' %
+                  (self._svcname(event.service, event.instance),
+                   self._fmt_state(event.state)))
+        if librl:
+            # Display a new prompt right now (unfortunately not exported by
+            # the readline module.)
+            librl.rl_forced_update_display()
+
+    def emptyline(self):
+        pass
+
+    def onecmd(self, line):
+        try:
+            return cmd.Cmd.onecmd(self, line)
+        except Exception as err:
+            print '\rError: %s' % err
+
+    def do_EOF(self, arg):
+        self.client.close()
+        return True
+    do_q = do_quit = do_EOF
+
+    def do_list(self, arg):
+        self.client.send(RequestServiceListCommand())
+    do_l = do_list
+
+    def do_start(self, arg):
+        self.client.send(StartCommand(*self._svcinst(arg)))
+
+    def do_stop(self, arg):
+        self.client.send(StopCommand(*self._svcinst(arg)))
+
+    def do_restart(self, arg):
+        self.client.send(RestartCommand(*self._svcinst(arg)))
+
+
+try:
+    Console().cmdloop()
 except KeyboardInterrupt:
     pass
